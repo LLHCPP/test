3cc3501fc771eb5a7e6c9325eb0d4588a65f123c
LLH <1272324274@qq.com>
2021-10-25 15:21:28 +0800

    手写线程池

--- ThreadPool.h
+++ ThreadPool.h
@@ -22,7 +22,43 @@ private:
     std::condition_variable ConLock;//条件锁
     std::atomic<bool> Running{true};//运行状态
     std::atomic<uint16_t> MoTaskNum{0};
+public:
+    explicit ThreadPool(uint16_t);
+
+    ~ThreadPool();
+
+    int FreeThreadNum();
+
+    int CountThreadNum();
+
+    void addThread(uint16_t);
+
+    template<typename F, typename...Args>
+    auto commit(F &&f, Args &&... args) -> std::future<decltype(f(args...))>;
 };
 
+ThreadPool::ThreadPool(uint16_t size = 1) {
 
+};
+
+ThreadPool::~ThreadPool() {
+
+}
+
+int ThreadPool::FreeThreadNum() {
+
+}
+
+int ThreadPool::CountThreadNum() {
+    return 0;
+}
+
+void ThreadPool::addThread(uint16_t) {
+
+}
+
+template<typename F, typename... Args>
+auto ThreadPool::commit(F &&f, Args &&... args) -> std::future<decltype(f(args...))> {
+    return nullptr;
+};
 #endif //TEST_THREADPOOL_H
